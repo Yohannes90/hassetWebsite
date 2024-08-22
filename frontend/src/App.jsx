7562741ef35f5a490ef6f6a294e88fda58540285
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-08-22 15:09:26 -0700

    updating App.jsx as general layout

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <Hero />
+
+      </main>
+
+      <Footer />
+    </>
+  )
+}
+
+export default App
